dc35d2f4cf4b0c3212057a85fd70dd872f972a9f
Leroy <4c65726f79@gmail.com>
2019-07-02 18:07:43 +0200

    learn swift again

--- colorpicker/ViewController.swift
+++ colorpicker/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testbtn: UIButton!
+    @IBOutlet weak var Text: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changetext(_ sender: Any) {
+        
+        let tempT = Text.text + "asd"
+        if Text.text == "yourStringasd" {
+            
+             Text.text = "yourString"
+        } else {
+           Text.text = Text.text + "asd"
+        }
+        
+    }
+    
 }
 
